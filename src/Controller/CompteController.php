3ab3f17379a4aa72bab99618c787c235f527eca6
tboutroux <boutrouxtheo@gmail.com>
2024-02-28 10:53:56 +0100

    Add Authentication Crud

---
+++ src/Controller/CompteController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Compte;
+use App\Form\CompteType;
+use App\Repository\CompteRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/compte')]
+class CompteController extends AbstractController
+{
+    #[Route('/', name: 'app_compte_index', methods: ['GET'])]
+    public function index(CompteRepository $compteRepository): Response
+    {
+        return $this->render('compte/index.html.twig', [
+            'comptes' => $compteRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_compte_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $compte = new Compte();
+        $form = $this->createForm(CompteType::class, $compte);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($compte);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_compte_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('compte/new.html.twig', [
+            'compte' => $compte,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_compte_show', methods: ['GET'])]
+    public function show(Compte $compte): Response
+    {
+        return $this->render('compte/show.html.twig', [
+            'compte' => $compte,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_compte_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Compte $compte, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(CompteType::class, $compte);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_compte_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('compte/edit.html.twig', [
+            'compte' => $compte,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_compte_delete', methods: ['POST'])]
+    public function delete(Request $request, Compte $compte, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$compte->getId(), $request->request->get('_token'))) {
+            $entityManager->remove($compte);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_compte_index', [], Response::HTTP_SEE_OTHER);
+    }
+
+    // Route permettant de se connecter
+    #[Route('/login', name: 'app_compte_login', methods: ['GET', 'POST'])]
+    public function login(): Response
+    {
+        return $this->render('compte/login.html.twig');
+    }
+
+    // Route permettant de se déconnecter
+    #[Route('/logout', name: 'app_compte_logout', methods: ['GET'])]
+    public function logout(): Response
+    {
+        return $this->render('compte/logout.html.twig');
+    }
+
+}
