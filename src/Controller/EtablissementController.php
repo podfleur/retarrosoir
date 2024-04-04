88c05f87e1af9bebdaccc12f06aa6766d234dcd9
tboutroux <boutrouxtheo@gmail.com>
2024-04-04 09:11:54 +0200

    29 : Abonnement à un établissement

--- src/Controller/EtablissementController.php
+++ src/Controller/EtablissementController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use App\Entity\Etablissement;
 use App\Entity\Format;
 use App\Entity\Photo;
+use App\Entity\Compte;
+use App\Entity\Abonnement;
 use App\Form\EtablissementType;
 use App\Repository\EtablissementRepository;
 use Doctrine\ORM\EntityManagerInterface;
@@ -12,6 +14,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 #[Route('/etablissement')]
 class EtablissementController extends AbstractController
@@ -81,10 +84,42 @@ class EtablissementController extends AbstractController
     }
 
     #[Route('/{id}', name: 'app_etablissement_show', methods: ['GET'])]
-    public function show(Etablissement $etablissement): Response
+    public function show(Etablissement $etablissement, EntityManagerInterface $em): Response
     {
+        $compte = $this->getUser();
+
+        // S'il y a une photo, on la convertit en base64
+        if ($etablissement->getPhotoId() !== null) {
+            $donneesPhoto = base64_encode(stream_get_contents($etablissement->getPhotoId()->getDonneesPhoto()));
+            $format = $etablissement->getPhotoId()->getFormatId()->getNom();
+        } else {
+            $donneesPhoto = null;
+            $format = null;
+        }
+
+        $nbAbonnes = count($em->getRepository(Abonnement::class)->findBy(['suivi_etablissement_id' => $etablissement]));
+        $nbCompteAssocie = count($em->getRepository(Compte::class)->findBy(['etablissement_id' => $etablissement]));
+
+        $abonne = false;
+
+        // On cherche à savoir si le compte connecté est abonné à l'établissement
+        $abonnement = $em->getRepository(Abonnement::class)->findOneBy([
+            'suiveur_id' => $compte,
+            'suivi_etablissement_id' => $etablissement
+        ]);
+
+        if ($abonnement) {
+            $abonne = true;
+        }
+
         return $this->render('etablissement/show.html.twig', [
             'etablissement' => $etablissement,
+            'donneesPhoto' => $donneesPhoto,
+            'format' => $format,
+            'compte' => $compte,
+            'nbAbonnes' => $nbAbonnes,
+            'nbCompteAssocie' => $nbCompteAssocie,
+            'abonne' => $abonne
         ]);
     }
 
@@ -116,4 +151,64 @@ class EtablissementController extends AbstractController
 
         return $this->redirectToRoute('app_etablissement_index', [], Response::HTTP_SEE_OTHER);
     }
+
+    #[Route('/associes/{id}', name: 'app_etablissement_associes', methods: ['GET'])]
+    public function associes(Etablissement $etablissement, EntityManagerInterface $em): Response
+    {
+        $comptes = $em->getRepository(Compte::class)->findBy(['etablissement_id' => $etablissement]);
+
+        return $this->render('etablissement/associes.html.twig', [
+            'comptes' => $comptes,
+            'etablissement' => $etablissement,
+        ]);
+    }
+
+    #[Route('/etablissement-subscribe/{id}', name: 'app_etablissement_subscribe')]
+    public function subscribe($id, EntityManagerInterface $em, UserInterface $user, Etablissement $etablissement): Response
+    {
+        $this->denyAccessUnlessGranted('ROLE_USER');
+
+        if (!$etablissement) {
+            throw $this->createNotFoundException('Compte non trouvé');
+        }
+
+        $abonnement = new Abonnement();
+        $abonnement->setSuiveurId($user);
+        $abonnement->setSuiviEtablissementId($etablissement);
+
+        $em->persist($abonnement);
+        $em->flush();
+
+        return $this->redirectToRoute('app_etablissement_show', ['id' => $id]);
+    }
+
+    // Je veux créer une route pour se désabonner d'un compte
+    #[Route('/etablissement-unsubscribe/{id}', name: 'app_etablissement_unsubscribe')]
+    public function unsubscribe($id, EntityManagerInterface $em, UserInterface $user): Response
+    {
+        $this->denyAccessUnlessGranted('ROLE_USER');
+
+        $compte = $user;
+
+        $compteToUnsubscribe = $em->getRepository(Compte::class)->find($compte);
+
+        if (!$compteToUnsubscribe) {
+            throw $this->createNotFoundException('Compte non trouvé');
+        }
+
+        $abonnement = $em->getRepository(Abonnement::class)->findOneBy([
+            'suiveur_id' => $compte,
+            'suivi_etablissement_id' => $id
+        ]);
+
+        if (!$abonnement) {
+            throw $this->createNotFoundException('Abonnement non trouvé');
+        }
+
+        $em->remove($abonnement);
+        $em->flush();
+
+        return $this->redirectToRoute('app_etablissement_show', ['id' => $id]);
+    }
+
 }
